cc6e35fc34c74cc41ca6cead397789c746d95834
David Gorender <davidgorender@gmail.com>
2018-11-16 12:45:54 -0300

    Working for m=0

--- byzantine.py
+++ byzantine.py
@@ -17,8 +17,17 @@ class Process:
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.sock.bind((ip, port))
 		self.value = ''
-		listener = threading.Thread(target=self.receive)
-		listener.start()
+		self.listener = threading.Thread(target=self.receive)
+		self.listener.start()
+
+	def decide(self):
+		print("Process", self.id, "has decided", self.value)
+
+	def construct_message(self, process_id, value):
+  		message = {}
+  		message["id"] = process_id
+  		message["value"] = value
+  		return json.dumps(message)
 
 	def send(self, ip, port, message):
 		self.sock.sendto(message.encode('utf-8'), (ip, port))
@@ -27,31 +36,28 @@ class Process:
 		for member in group:
 			self.send(member["ip"], member["port"], message)
 
-	def construct_message(self, process_id, value):
-  		message = {}
-  		message["id"] = process_id
-  		message["value"] = value
-  		return json.dumps(message)
-
 	def receive(self):
 		while True:
-		    data, addr = self.sock.recvfrom(1024)
-		    print(self.id, "Message: ", data)
+		    message, addr = self.sock.recvfrom(1024)
+		    message = json.loads(message.decode('utf-8'))
+		    self.handle_message(message)
+
+	def handle_message(self, message):
+		if m == 0:
+			self.value = message["value"]
+			self.decide()
+		else:
+			#Handle data
+			pass
 
 	def oral_messages(self, m):
-		if m == 0:
-			#Commander
-			if self.id == 0:
-				self.value = 'attack'
-				message = self.construct_message(self.id, self.value)
-				self.multicast(self.process_list[1:], message)
-
-	def run(self):
+		#Commander
 		if self.id == 0:
-			self.oral_messages(m)
+			self.value = 'attack'
+			message = self.construct_message(self.id, self.value)
+			self.multicast(self.process_list[1:], message)
 
 processes_info = [{"ip":"localhost", "port":9000 + i, "id":i} for i in range(n)]
 processes = [Process(process["ip"], process["port"], process["id"], processes_info) for process in processes_info]
 
-for p in processes:
-	p.run()
+processes[0].oral_messages(m)
