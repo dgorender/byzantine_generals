95ab5882100be97078b6375efad06c588148fa50
David Gorender <davidgorender@gmail.com>
2018-11-18 11:15:40 -0300

    Fixing never stopping thread

--- byzantine.py
+++ byzantine.py
@@ -5,11 +5,14 @@ import json
 import random
 from collections import Counter
 from anytree import AnyNode, RenderTree, Resolver, LevelOrderIter
+from anytree.exporter import DotExporter
 
+#Read input
 n, m, is_commander_traitor = [int(x) for x in argv[1:]]
 if (n < 3*m + 1) or (is_commander_traitor == 1 and m == 0):
 	print("It is impossible to solve the byzantine generals problem for that configuration")
 
+#Majority function
 def majority(values):
 	counter = Counter(values)
 	maj = counter.most_common()
@@ -27,7 +30,10 @@ class Process:
 		self.process_list = process_list
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.sock.bind((ip, port))
-		self.value = ''
+		if process_id == "0":
+			self.value = "attack"
+		else:
+			self.value = ''
 		self.loyal = True
 		self.root = None
 		self.listener = threading.Thread(target=self.receive)
@@ -55,31 +61,37 @@ class Process:
 			self.send(member["ip"], member["port"], message)
 
 	def receive(self):
-		while True:
+		while self.value == '':
 		    message, addr = self.sock.recvfrom(1024)
 		    message = json.loads(message.decode('utf-8'))
 		    self.handle_message(message)
 
 	def handle_message(self, message):
+		#m = 0
 		if m == 0:
 			self.value = message["value"]
 			self.decide()
+		#m > 0
 		else:
 			path = message["path"]
-			#Handle data
 			#Commander message, empty tree
 			if len(message["path"]) == 1:
-				self.root = AnyNode(id=path[0], value=message["value"], decide_value=message["value"])
+				self.root = AnyNode(id=path[0], value=message["value"], decide_value=message["value"], name=path[0])
+			#Message from a lieutenant
 			else:
+				#Get last known node on path and add new node as its child
 				r = Resolver('id')
 				last_node = r.get(self.root, '/'.join(path[1:-1]))
-				AnyNode(id=path[-1], value=message["value"], decide_value=message["value"], parent=last_node)
+				AnyNode(id=path[-1], value=message["value"], decide_value=message["value"], parent=last_node, name=path[-1])
 			if self.id not in path and len(path) <= m:
+				#If possible, apend its own id to the path and broadcast the message
 				path.append(self.id)
 				message = self.construct_message(path, message["value"])
 				self.multicast(self.process_list[1:], message)
 			leafs = [node for node in LevelOrderIter(self.root) if node.is_leaf]
-			if len(leafs) == (n - 1) * (n - m):
+
+			if (len(leafs) == (n - 1) * (n - m)) or (m == 1 and len(leafs) == (n - 1)):
+				#If already received all the messages, start phase 2
 				reverse_nodes = [node for node in LevelOrderIter(self.root)][::-1]
 				for node in reverse_nodes:
 					if not node.is_leaf:
@@ -87,11 +99,12 @@ class Process:
 				self.value = self.root.decide_value
 				self.decide()
 				print(self.id, RenderTree(self.root))
+				#DotExporter(self.root, nodeattrfunc=lambda node: 'label="id: {} | value: {} | decide_value: {}"'.format(node.id, node.value, node.decide_value)).to_picture("./results/" + self.id + ".png")
+				
 
 	def oral_messages(self, m):
 		#Commander
 		if self.id == "0":
-			self.value = 'attack'
 			message = self.construct_message([self.id], self.value)
 			self.multicast(self.process_list[1:], message)
 
